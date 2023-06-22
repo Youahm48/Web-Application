1f237d6d7cbe0d6b38e8dac53465576d9bae500d
Youahm48 <134890087+Youahm48@users.noreply.github.com>
2023-06-22 22:49:13 +0000

    Sidebar split into external and internal

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,17 +2,31 @@ import React from "react"
 import "../stylesheets/components/Sidebar.css"
 
 export default function Sidebar() {
+  function expand() {
+    
+  }
+
+  function collapse() {
+    
+  }
+  
   return(
     <div id="sidebar">
-      <button id="sidebar-toggle" className="material-symbols-outlined">menu</button>
-      <div className="sidebar-section">
-        <a className="material-symbols-outlined sidebar-item">home</a>
-        <a className="material-symbols-outlined sidebar-item">check_circle</a>
-        <a className="material-symbols-outlined sidebar-item">calendar_month</a>
+      <div id="sidebar-internal">
+        <div>Title&Logo</div>
+        <div>Dashboard</div>
       </div>
-      <div id="bottom-sidebar-section" className="sidebar-section">
-        <a className="material-symbols-outlined sidebar-item">help</a>
-        <a className="material-symbols-outlined sidebar-item">settings</a>
+      <div id="sidebar-external">
+        <button id="sidebar-toggle" className="material-symbols-outlined">menu</button>
+        <div className="sidebar-section">
+          <a className="material-symbols-outlined sidebar-item">home</a>
+          <a className="material-symbols-outlined sidebar-item">check_circle</a>
+          <a className="material-symbols-outlined sidebar-item">calendar_month</a>
+        </div>
+        <div id="bottom-sidebar-section" className="sidebar-section">
+          <a className="material-symbols-outlined sidebar-item">help</a>
+          <a className="material-symbols-outlined sidebar-item">settings</a>
+        </div>
       </div>
     </div>
   )
