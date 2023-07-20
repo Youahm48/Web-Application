7ce5cdf7538548e7d4032201d0c1168e97b9b37c
Youahm48 <134890087+Youahm48@users.noreply.github.com>
2023-07-20 02:07:29 +0000

    Express server setup

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
+const path = require("path")
 const express = require("express")
 
 const port = process.env.PORT || 3000
 
-const server = express()
+const app = express()
 
-server.listen(port, function() {
-  console.log(`Server listening on port ${port}`)
+app.use(express.static(path.resolve(__dirname, "../dist")))
+
+app.get("*", function(req, res) {
+  res.sendFile(path.resolve(__dirname, "../dist", "index.html"))
 })
 
-/*ReferenceError: require is not defined in ES module scope, you can use import instead
-This file is being treated as an ES module because it has a '.js' file extension and '/home/runner/Web-Application/package.json' contains "type": "module". To treat it as a CommonJS script, rename it to use the '.cjs' file extension.*/
+app.listen(port, function() {
+  console.log(`Server listening on port ${port}`)
+})
