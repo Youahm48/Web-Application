37f460ed7442a0d792ee6ba8f3500b2d51aaaaf3
Youahm48 <134890087+Youahm48@users.noreply.github.com>
2023-07-23 03:33:16 +0000

    Added test data (previous commit was axios not axis)

---
+++ server/dbInit.js
@@ -0,0 +1,38 @@
+/*
+Test Data
+*/
+
+const Database = require("./Database.js")
+
+const userId = "defaultuser"
+const databaseModel = {
+  password: "r",
+  taskList: [
+    {
+      deadline: "2023-07-23T03:21:00.000Z",
+      id: "Task-Mz9dEvlFitDEJ-eQSv8Lt",
+      isComplete: false,
+      name: "a",
+      priority: "Low",
+      subject: "z"
+    },
+    {
+      deadline: "2023-07-22T03:21:00.000Z",
+      id: "Task-e2Fss5P8gUVIwuWaO29JZ",
+      isComplete: false,
+      name: "b",
+      priority: "Low",
+      subject: "y",
+    },
+    {
+      deadline: "2023-07-24T03:22:00.000Z",
+      id: "Task-AYzh-tPvKD3Nzspe33f4b",
+      isComplete: false,
+      name: "c",
+      priority: "Low",
+      subject: "x"
+    }
+  ],
+}
+
+Database.define(userId, databaseModel)
