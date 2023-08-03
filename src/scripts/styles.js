450b0959ebfbe3e24935514a92162978400f66c5
Youahm48 <134890087+Youahm48@users.noreply.github.com>
2023-08-03 00:09:42 +0000

    Created function to change background colour on mouseover of both internal and external corresponding sidebar elements

--- src/scripts/styles.js
+++ src/scripts/styles.js
@@ -1,8 +1,20 @@
 //Simultaneous highlighting of internal and external sidebar items
 //get elements which have class name ending with "-menu-item" then filter through each element and element on hover apply hover stylings to the element's menu item class. I may need to use regex for the first part.
 
+/*Remember to leave at 12:10*/
+
 export default function jsStyles() {
+  
+  
   document.querySelectorAll('[class$="-menu-item"]').forEach(function(element) {
-    console.log(element)
+    element.addEventListener("mouseover", function(event) {
+      var className = Array.from(event.target.classList).filter((item) => item.endsWith("-menu-item"))
+      Array.from(document.getElementsByClassName(className)).forEach(function(e) {
+        e.style.backgroundColor = "#2f2f2f"
+      })
+    })
+    //console.log(element.classList.value)
+    //on hover select other elements with same class and append new class with hover stylings
+    //Make function to remove stylings on mouseout
   })
 }
