8b28a4abb68d39fb790b4f7941c3725dda4426bc
Youahm48 <134890087+Youahm48@users.noreply.github.com>
2023-08-22 01:14:33 +0000

    Added login button to header and added alternative bg styling comment for main

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,9 @@ export default function Header() {
     <div id="header">
       <img id="logo" src="/logo.png"/>
       <div id="title">Title</div>
+      <div id="profile-container">
+        <a href="/register" id="login-button">Log in</a>
+      </div>
     </div>
   )
 }
