c9f072414f4392fbfbf6579ff2a383206203aa55
Youahm48 <134890087+Youahm48@users.noreply.github.com>
2023-08-22 01:13:24 +0000

    Created registration page and added a few styles

---
+++ src/routes/Register.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+
+import Header from "../components/Header"
+
+import "../stylesheets/routes/Register.css"
+
+export default function Documentation(props) {
+  return(
+      <div id="vertical-container">
+        <Header />
+        <div id="main">
+          <div id="form-container">
+            <div id="form">
+              <div id="form-header">
+                <img id="logo-spin" src="/logo.png" />
+              </div>
+              <div id="main-form">
+                <div id="tabs">
+                  <div id="login-tab">Log in</div>
+                  <div id="registration-tab">Register</div>
+                </div>
+                <div id="login-form">
+                  <p>Username</p>
+                  <input id="username"></input>
+                  <p>Password</p>
+                  <input id="password"></input>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
