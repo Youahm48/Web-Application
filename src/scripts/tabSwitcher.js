4a4c10121dbd6b57e359699522370e088e844b87
Youahm48 <134890087+Youahm48@users.noreply.github.com>
2023-08-27 03:41:16 +0000

    Updated tab switcher function

--- src/scripts/tabSwitcher.js
+++ src/scripts/tabSwitcher.js
@@ -1,15 +1,24 @@
-export default function jsTabSwitcher() {
+export default function jsTabSwitcher(setLoginUsername, setLoginPassword, setRegisterUsername, setRegisterPassword) {
   const loginTab = document.getElementById("login-tab")
   const registerTab = document.getElementById("registration-tab")
 
   const loginForm = document.getElementById("login-form")
   const registerForm = document.getElementById("registration-form")
+
+  const loginUsername = document.getElementById("login-username")
+  const loginPassword = document.getElementById("login-password")
+  const registerUsername = document.getElementById("register-username")
+  const registerPassword = document.getElementById("register-password")
     
   loginTab.onclick = function() {
     loginTab.classList.remove("inactive")
     registerTab.classList.add("inactive")
     loginForm.style.display = "flex"
     registerForm.style.display = "none"
+    setRegisterUsername("")
+    setRegisterPassword("")
+    registerUsername.reset()
+    registerPassword.reset()
   }
 
   registerTab.onclick = function() {
@@ -17,5 +26,9 @@ export default function jsTabSwitcher() {
     registerTab.classList.remove("inactive")
     loginForm.style.display = "none"
     registerForm.style.display = "flex"
+    setLoginUsername("")
+    setLoginPassword("")
+    loginUsername.reset()
+    loginPassword.reset()
   }
 }
